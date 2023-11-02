9bf0f4e952a5e7172ec02b7d4865af45d1eac45d
lancew <lw@judocoach.com>
2023-11-02 20:47:07 +0000

    Initial version

---
+++ lib/WebService/Readwise.pm
@@ -0,0 +1,59 @@
+use strict;
+use warnings;
+
+use v5.010;
+
+package WebService::Readwise;
+
+# ABSTRACT: Perl module to interact with Readwise.io API:w
+use HTTP::Tiny;
+use JSON::MaybeXS;
+
+use Moo;
+use namespace::clean;
+
+has token => (
+    is       => 'ro',
+    required => 0,
+    default  => sub { return $ENV{WEBSERVICE_READWISE_TOKEN} },
+);
+
+has base_url => (
+    is      => 'ro',
+    default => sub {'https://readwise.io/api/v2/'},
+);
+
+has http => (
+    is      => 'ro',
+    default => sub {
+        return HTTP::Tiny->new;
+    },
+);
+
+sub auth {
+    my $self = shift;
+
+    my $response = $self->http->request( 'GET', $self->base_url . 'auth/',
+        { headers => { Authorization => "Token $self->{token}", }, } );
+
+    return $response->{status};
+}
+
+sub highlights {
+    my $self = shift;
+
+    my $response = $self->http->request( 'GET', $self->base_url . 'highlights/',
+        { headers => { Authorization => "Token $self->{token}", }, } );
+
+    
+    if ( !$response->{success} ) {
+        return 'Response error';
+    }
+
+    my $json = decode_json $response->{content};
+
+    return $json;
+}
+
+
+1;
